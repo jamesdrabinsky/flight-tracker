1f1e0b204ea11314ffd32432f5528c3ff994362a
James Drabinsky <james.drabinsky@gmail.com>
2023-06-15 21:13:14 -0700

    Adding singup form and related routes.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "2.7.7"
+ruby "2.7.5"
 
 gem "sinatra", "~>1.4.7"
 gem "sinatra-contrib"
--- flight_tracker.rb
+++ flight_tracker.rb
@@ -15,8 +15,18 @@ configure do
 end
 
 configure(:development) do 
-  # require "sinatra/reloader"
+  require "sinatra/reloader"
   # also_reload "database_persistence.rb"
+end
+
+helpers do 
+  def flash_error_formatting(session_errors)
+    if session_errors.is_a?(Array)
+      "<br>" + session_errors.join("<br>")
+    else
+      session_errors
+    end
+  end
 end
 
 def require_signed_in_user
@@ -26,21 +36,40 @@ def require_signed_in_user
   end
 end
 
-def load_user_credentials
+def hash_passwords
   connection = PG.connect(dbname: "flights")
-  result = connection.exec("SELECT * FROM users;")
+  passwords = connection.exec("SELECT password FROM users;").map { |tuple| tuple["password"] }
+  hashed_passwords = passwords.map { |pass| BCrypt::Password.create(pass) }
+  connection.exec("UPDATE users SET password = '#{hashed_passwords[0]} WHERE id = 1;")
+  connection.exec("UPDATE users SET password = '#{hashed_passwords[1]}' WHERE id = 2;")
+end
+
+def load_user_credentials(username)
+  connection = PG.connect(dbname: "flights")
+  result = connection.exec_params("SELECT * FROM users WHERE username = $1", [username])
+  tuple = result.first
   
-  result.map do |tuple|
-    { first_name: tuple["first_name"], 
-      last_name: tuple["last_name"], 
-      username: tuple["username"], 
-      password: tuple["password"] }
-  end  
+  { first_name: tuple["first_name"], 
+    last_name: tuple["last_name"], 
+    username: tuple["username"], 
+    password: tuple["password"] }
 end
 
 def valid_credentials?(username, password)
-  credentials = load_user_credentials
-  BCrypt::Password.new(credentials[username]) == password
+  credentials = load_user_credentials(username)
+  BCrypt::Password.new(credentials[:password]) == password
+end
+
+def validate_registration(params)
+  first_name, last_name, username, password = params.values.map(&:strip)
+  session[:error] = []
+
+  session[:error] << "First names can only contain alpha characters" if !(first_name =~ /^[a-z]+$/i)
+  session[:error] << "Last names must contain alpha characters and an optional '-' or space" if !(last_name =~ /^[a-z]+( |-)?[a-z]*$/i)
+  session[:error] << "Username can only contain alpha and numeric characters" if !(username =~ /^[a-z0-9]+$/i)
+  session[:error] << "Password can only contain alpha and numeric characters and must be at least 6 characters" if !(password =~ /^[a-z0-9]{6,}$/i)
+
+  session.delete(:error) if session[:error].empty?
 end
 
 get "/" do 
@@ -52,22 +81,41 @@ get "/users/signin" do
   erb :signin, layout: :layout
 end
 
-# Render the new flight form
-get "/flight/new" do
-  erb :new_list, layout: :layout
-end
-
 # User signin validation
 post "/users/signin" do
-  username, password = params.values_at(:username, :password)
+  username, password = params.values
 
   if valid_credentials?(username, password)
     session[:username] = username
-    session[:success] = "Welcome!"
+    session[:success] = "You are signed in"
     redirect "/"
   else
     session[:error] = "Invalid credentials"
     status 422
-    erb :signin
+    erb :signin, layout: layout
   end
 end
+
+get "/register" do
+  erb :register, layout: :layout
+end
+
+post "/register" do 
+  validate_registration(params)
+
+  # binding.pry
+
+  if session[:error]
+    status 422
+    erb :register, layout: :layout
+    pry
+  else
+    session[:success] = "You are registered"
+    redirect "/users/signin"
+  end
+end
+
+# # Render the new flight form
+# get "/flight/new" do
+#   erb :new_list, layout: :layout
+# end
